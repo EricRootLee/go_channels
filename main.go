2679b9cbfca403862ef7a73ef55a743dc23f0589
EricRootLee <erickiokom@gmail.com>
2022-04-20 17:32:13 +0300

    channels

--- main.go
+++ main.go
@@ -9,26 +9,35 @@ func main() {
 
 	channel1 := make(chan string)
 	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
 
 	go func() {
 		for {
-			channel1 <- "Hey channel 1 working "
-			time.Sleep(2 * time.Second)
+			time.Sleep(3 * time.Second)
+			channel1 <- "Hey, channel 1 working - after  3 secs "
 		}
 
 	}()
 	go func() {
 		for {
-			channel2 <- "Hey channel 2 working "
-			time.Sleep(3 * time.Second)
+			time.Sleep(7 * time.Second)
+			channel2 <- "Hey, channel 2 working - after  7 secs "
 		}
-
 	}()
 
 	for {
-
-		fmt.Println(<-channel1)
-		fmt.Println(<-channel2)
-
+		select {
+		case <-channel1:
+			fmt.Println(<-channel1)
+		case <-channel2:
+			fmt.Println(<-channel2)
+		case <-time.After(14 * time.Second):
+			fmt.Println("Bye")
+			return
+		default:
+			fmt.Println("Waiting...")
+			time.Sleep(1 * time.Second)
+		}
 	}
 }
